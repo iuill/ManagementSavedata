8ab7b894f316c11e1e6116ed680f05aeaa0e8592
iuill <jhdkoiiondt@yahoo.co.jp>
2020-12-06 19:52:31 +0900

    昔作ったものを追加

---
+++ src/ダークソウルbackupツール/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ダークソウルbackupツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "data dir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "bak dir";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "%APPDATA%\\DarkSoulsIII";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 19);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "H:\\ダークソウル3_databackup";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "復元";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(148, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(283, 184);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(275, 158);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(275, 158);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(3, 3);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(269, 152);
+            this.textBox3.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 161);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(283, 23);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(134, 18);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(134, 18);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 184);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "ダークソウルバックアップツール";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ src/ダークソウルbackupツール/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ダークソウルbackupツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dataDir = System.Environment.ExpandEnvironmentVariables(this.textBox1.Text);
+            string bakDir = this.textBox2.Text;
+
+            if (!Directory.Exists(dataDir))
+            {
+                return;
+            }
+            if (!Directory.Exists(bakDir))
+            {
+                return;
+            }
+
+            this.comboBox1.Items.AddRange(this.getBackupNodes(this.textBox2.Text));
+
+            this.toolStripStatusLabel1.Text = "";
+            this.toolStripStatusLabel2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dataDir = System.Environment.ExpandEnvironmentVariables(this.textBox1.Text);
+            string bakDir = this.textBox2.Text;
+
+            if (!Directory.Exists(dataDir))
+            {
+                MessageBox.Show(this.label1.Text + " のフォルダは存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!Directory.Exists(bakDir))
+            {
+                MessageBox.Show(this.label2.Text + " のフォルダは存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            this.toolStripStatusLabel2.Text = "■■[executing]■■■■";
+            this.Refresh();
+
+            DateTime now = DateTime.Now;
+            string nodeDir = now.ToString("yyyyMMddHHmm");
+            string destDir = Path.Combine(bakDir, nodeDir, Path.GetFileNameWithoutExtension(dataDir));
+            Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory(dataDir, destDir, true);
+
+            this.comboBox1.Items.Insert(0, nodeDir);
+            this.comboBox1.SelectedIndex = -1;
+
+            this.textBox3.AppendText(now.ToString("yyyy/MM/dd HH:mm:ss") + " | [backup] @" + nodeDir + System.Environment.NewLine);
+            this.toolStripStatusLabel2.Text = "[backup] @" + nodeDir;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string dataDir = System.Environment.ExpandEnvironmentVariables(this.textBox1.Text);
+            string bakDir = this.textBox2.Text;
+
+            if (!Directory.Exists(dataDir))
+            {
+                MessageBox.Show(this.label1.Text + " のフォルダは存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!Directory.Exists(bakDir))
+            {
+                MessageBox.Show(this.label2.Text + " のフォルダは存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (this.comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("復元対象の日時を選択してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            this.toolStripStatusLabel2.Text = "■■[executing]■■■■";
+            this.Refresh();
+
+            string nodeFullDir = Path.Combine(bakDir, this.comboBox1.Text, Path.GetFileNameWithoutExtension(dataDir));
+            Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory(nodeFullDir, dataDir, true);
+
+            this.textBox3.AppendText(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " | [recovery] @" + this.comboBox1.Text + System.Environment.NewLine);
+            this.toolStripStatusLabel2.Text = "[recovery] @" + this.comboBox1.Text;
+        }
+
+        private string[] getBackupNodes(string bakDir){
+            string[] childDirs = System.IO.Directory.GetDirectories(bakDir, "*", System.IO.SearchOption.TopDirectoryOnly);
+
+            var query = (from s in childDirs
+                        orderby s descending
+                        select System.IO.Path.GetFileNameWithoutExtension(s));
+
+            string[] ret = query.ToArray<string>();
+            return ret;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.toolStripStatusLabel1.Text = DateTime.Now.ToString("MM-dd HH:mm:ss");
+        }
+    }
+}
